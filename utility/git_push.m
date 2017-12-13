18dd2179441268e83cf95c434b53c034b5951847
HiroMiyawaki <miyawaki@dhcp-sch-f15-223-54.med.osaka-cu.ac.jp>
2017-12-13 18:09:44 +0900

    comit at 13-Dec-2017 17:58:15

--- utility/git_push.m
+++ utility/git_push.m
@@ -2,17 +2,23 @@ function git_push(varargin)
 
 
     param.gitPath='/usr/local/bin/git';
-    param.localPath='~/Documents/MATLAB/matlab_tools/';
+    param.repoPath='~/Documents/MATLAB/matlab_tools/';
     param.comment=['comit at ' datestr(now) ];
     
     
     paramNames=fieldnames(param);
     
+    if mod(length(varargin),2)==1
+        error('option must be a pair of name and its value');
+    end
+        
     for n=1:length(varargin)/2        
-        name=paramNames(strcmpi(paramNames,a));
+        name=paramNames(strcmpi(paramNames,varargin{2*n-1}));
         if isempty(name)
-        
-        
+            error('Wrong option')
+        else
+            param.(name)=varargin{2*n};
+        end
     end
     
     eval(['!' param.gitPath ' -C ' param.repoPath ' add --all'])
