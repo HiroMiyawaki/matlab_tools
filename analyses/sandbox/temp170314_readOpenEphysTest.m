cd3e6b067f9e1ccae5b0683f404321ec8646a35a
HiroMiyawaki <miyawaki@dhcp-sch-f15-223-54.med.osaka-cu.ac.jp>
2017-12-14 09:00:30 +0900

    organize folders

--- analyses/sandbox/temp170314_readOpenEphysTest.m
+++
@@ -1,7 +0,0 @@
-clear
-
-fileName='/Volumes/USBMEM/openephys_2017-03-14_14-18-17/110_CH1.continuous';
-
-
-[data, timestamps, info]=load_open_ephys_data_faster(fileName);
-plot((1:length(data))/info.header.sampleRate,data,'k-')
