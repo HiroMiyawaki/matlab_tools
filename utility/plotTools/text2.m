2a68ea7c7240d36c0cf0465436b00b46e7547007
HiroMiyawaki <miyawaki625@gmail.com>
2018-04-11 11:30:34 +0900

    update text2

--- utility/plotTools/text2.m
+++ utility/plotTools/text2.m
@@ -1,17 +1,28 @@
-function h=text2(Xpos,Ypos,String,Axis,Property)
+% function h=text2(Xpos,Ypos,String,Axis,varargin)
+% wrapper of text to specify text as in relative position
+%
+% Hiro Miyawaki at UWM, 2011-
+%
+function h=text2(Xpos,Ypos,String,Axis,varargin)
 
     if nargin<4
         Axis=axis();
     end
-    if nargin<5
-        Property={};
-    end
-
     
+    % just for backward compatibility
+    if length(varargin)==1 && iscell(varargin)
+        Property=varargin{1};
+    else
+        Property=varargin;
+    end
 
     H=text(Axis(1:2)*[1-Xpos;Xpos],Axis(3:4)*[1-Ypos;Ypos],String,Property{:});
     if nargout>0
         h=H;
     end
+    
+    
+    
+    
     
     
