8acdd7290d6cbc246673fd10d9e8a1d5372f8678
HiroMiyawaki <miyawaki625@gmail.com>
2019-01-08 11:37:34 +0900

    comit at 08-Jan-2019 11:37:34

--- utility/plotTools/initFig4A4.m
+++ utility/plotTools/initFig4A4.m
@@ -1,36 +1,56 @@
-function fh=initFig4A4()
+function fh=initFig4A4(varargin)
 
-if nargin <1
-    nColumn=1;
-end
 
+param.landscape=false;
+param.fontsize=10;
+param.markerSize=12;
+param.lineWidth=1;
+param.margin=0;
+param=parseParameters(param,varargin);
+param.windowBottom=1000;
+param.windowLeft=0;
+%%
 % expand=5.5;
-expand=3.6;
+expand=3;
 
 width=21.0;
 height=29.7;
 
 
+if param.landscape
+    temp=width;
+    width=height;
+    height=temp;
+    clear temp    
+    paperOrient='landscape';
+else
+   paperOrient='portrait';
+end
 
-
-fh=figure('position',[-1000,1000,width*10*expand,height*10*expand]);
-set(fh, 'paperUnit','centimeters')
-set(fh,'PaperOrientation','portrait')
+% fh=figure('position',[-1000,1000,(width-param.margin*2)*10*expand,(height-param.margin*2)*10*expand]);
+% fh=figure('position',[-1000,1000,width*10*expand,height*10*expand]);
+fh=figure();
+set(fh, 'paperUnit','centimeters','Units','centimeters')
+% set(fh,'position',[0,1000,width,height]);
+set(fh,'position',[param.windowLeft,param.windowBottom,(width-param.margin*2),(height-param.margin*2)])
+set(fh, 'Units','centimeters')
+set(fh,'PaperOrientation',paperOrient)
 set(fh,'PaperType','a4')
-set(fh,'paperPosition',[0,0,width,height])
+set(fh,'paperPosition',[param.margin,param.margin,width-param.margin,height-param.margin])
+% set(fh,'paperPosition',[0,0,width,height])
 
 set(fh,'defaultAxesFontName','Helvetica')
 set(fh,'defaultTextFontName','Helvetica')
  
-set(fh,'defaultAxesFontSize',10);
-set(fh,'defaultTextFontSize',10);
-set(fh,'defaultAxesLineWidth', 1);
-set(fh,'defaultLineLineWidth', 1); 
-set(fh,'DefaultLineMarkerSize', 12); 
+set(fh,'defaultAxesFontSize',param.fontsize);
+set(fh,'defaultTextFontSize',param.fontsize);
+set(fh,'defaultAxesLineWidth', param.lineWidth);
+set(fh,'defaultLineLineWidth', param.lineWidth); 
+set(fh,'DefaultLineMarkerSize', param.markerSize); 
 
 set(fh,'DefaultAxesXGrid','off');
 set(fh,'DefaultAxesYGrid','off');
 set(fh,'DefaultAxesBox','off');
-% set(fh,'PaperPositionMode','auto');
+set(fh,'PaperPositionMode','auto');
 
 end
