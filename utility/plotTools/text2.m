429b6c61c490820d42e68d7754564b35d367ee3e
HiroMiyawaki <miyawaki625@gmail.com>
2019-01-21 11:32:21 +0900

    comit at 21-Jan-2019 11:32:21

--- utility/plotTools/text2.m
+++ utility/plotTools/text2.m
@@ -16,7 +16,17 @@ function h=text2(Xpos,Ypos,String,Axis,varargin)
         Property=varargin;
     end
 
-    H=text(Axis(1:2)*[1-Xpos;Xpos],Axis(3:4)*[1-Ypos;Ypos],String,Property{:});
+    if strcmpi(get(gca,'XScale'),'log')
+        convPosX=exp(log(Axis(1:2))*[1-Xpos;Xpos]);
+    else
+        convPosX=Axis(1:2)*[1-Xpos;Xpos];
+    end
+    if strcmpi(get(gca,'yScale'),'log')
+        convPosY=exp(log(Axis(3:4))*[1-Ypos;Ypos]);
+    else
+        convPosY=Axis(3:4)*[1-Ypos;Ypos];      
+    end
+    H=text(convPosX,convPosY,String,Property{:});
     if nargout>0
         h=H;
     end
