6f287ef17005661f947fd281c25f615913a4341b
HiroMiyawaki <miyawaki625@gmail.com>
2019-12-03 08:41:30 +0900

    comit at 03-Dec-2019 08:41:30

---
+++ utility/tools/uniqueCellRows.m
@@ -0,0 +1,19 @@
+function varargout=uniqueCellRows(x)
+
+[list,~,id]=unique(x);
+id=reshape(id,size(x));
+
+[rowList,firstIndex,rowID]=unique(id,'rows')
+
+if nargout>0
+    varargout{1}=list(rowList);
+end
+
+if nargout>1
+    varargout{2}=firstIndex;
+end
+
+if nargout>2
+    varargout{3}=rowID;
+end
+
