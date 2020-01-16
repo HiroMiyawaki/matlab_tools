cca5d6d5f95718ba87b59e0b96abf8282e5094f4
HiroMiyawaki <miyawaki625@gmail.com>
2020-01-16 12:25:35 +0900

    comit at 16-Jan-2020 12:25:35

--- utility/plotTools/savePDFmulti.m
+++ utility/plotTools/savePDFmulti.m
@@ -17,6 +17,10 @@ function savePDFmulti(fhList,pdfFile,varargin)
     param.vector=true;
     param.resolution=300;
     param.catPDFoptions={};
+    param.pngPage=[];
+    param.convertPath='/usr/local/bin/convert';
+    
+    param=parseParameters(param,varargin);
 
     if param.vector
         renderer='-painters';
@@ -40,4 +44,35 @@ function savePDFmulti(fhList,pdfFile,varargin)
     
     catPDF(pdfFile,pdfList,'remove',true,param.catPDFoptions{:})
     rmdir(tempDir,'s');
+    
+    if ~isempty(param.pngPage)
+        [filePath,fileName,~]=fileparts(pdfFile)
+        
+        if ~exist(fullfile(filePath,'png'),'dir')
+            mkdir(fullfile(filePath,'png'))
+        end
+        if ~exist(fullfile(filePath,'jpeg'),'dir')
+            mkdir(fullfile(filePath,'jpeg'))
+        end
+        
+        for n=1:length(param.pngPage)  
+            
+            if n==1 && length(fhList)==1
+                postFix='';
+            else                
+                postFix=sprintf('_page%02d',n);
+            end
+            
+            orient(fhList(param.pngPage(n)),'portrait')
+            print(fhList(param.pngPage(n)),fullfile(filePath,'png',sprintf('%s%s.png',fileName,postFix)),'-dpng',resolution)
+            
+            command=sprintf('%s %s.png -quality 60 %s.jpeg',...
+                param.convertPath,...
+                fullfile(filePath,'png',sprintf('%s%s',fileName,postFix)),...
+                fullfile(filePath,'jpeg',sprintf('%s%s',fileName,postFix)));
+            system(command);
+        end
+    end
+
+    
 end
