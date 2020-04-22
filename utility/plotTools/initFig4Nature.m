85cee45f43c92628958666610dff5bf184287d15
HiroMiyawaki <miyawaki625@gmail.com>
2020-04-22 13:01:41 +0900

    comit at 22-Apr-2020 13:01:41

--- utility/plotTools/initFig4Nature.m
+++ utility/plotTools/initFig4Nature.m
@@ -1,40 +1,57 @@
-function fh=initFig4Nature(nColumn)
+function fh=initFig4Nature(varargin)
 
-if nargin <1
-    nColumn=1;
-end
+param.landscape=false;
+param.fontsize=5;
+param.markerSize=9;
+param.lineWidth=0.5;
+param.nColumn=2;
+param.margin=0;
+param.windowBottom=20;
+param.windowLeft=0;
+param.axesColor=[0,0,0];
+%%
+param=parseParameters(param,varargin);
 
-% expand=5.5;
-expand=3.6;
-if nColumn==2
-    width=18.3;
-    height=24.7;
-else
+%%
+expand=3;
+
+if param.nColumn==1
     width=8.9;
-    height=24.7;
-end    
+else
+    width=18.3;
+end
+height=24.7;
 
+paperOrient='portrait';
 
-
-
-fh=figure('position',[-1000,1000,width*10*expand,height*10*expand]);
-set(fh, 'paperUnit','centimeters')
-set(fh,'PaperOrientation','portrait')
-set(fh,'PaperType','usletter')
-set(fh,'paperPosition',[(21.6-18.3)/2,(27.9-24.7)/2,width,height])
+% fh=figure('position',[-1000,1000,(width-param.margin*2)*10*expand,(height-param.margin*2)*10*expand]);
+% fh=figure('position',[-1000,1000,width*10*expand,height*10*expand]);
+fh=figure();
+set(fh, 'paperUnit','centimeters','Units','centimeters')
+% set(fh,'position',[0,1000,width,height]);
+set(fh,'position',[param.windowLeft,param.windowBottom,(width-param.margin*2),(height-param.margin*2)])
+set(fh, 'Units','centimeters')
+set(fh,'PaperOrientation',paperOrient)
+set(fh,'PaperType','a4')
+set(fh,'paperPosition',[param.margin,param.margin,width-param.margin,height-param.margin])
+% set(fh,'paperPosition',[0,0,width,height])
 
 set(fh,'defaultAxesFontName','Helvetica')
 set(fh,'defaultTextFontName','Helvetica')
- 
-set(fh,'defaultAxesFontSize',5);
-set(fh,'defaultTextFontSize',5);
-set(fh,'defaultAxesLineWidth', 1);
-set(fh,'defaultLineLineWidth', 1); 
-set(fh,'DefaultLineMarkerSize', 12); 
+
+set(fh,'defaultAxesXColor',param.axesColor); % factory is [0.15,0.15,0.15]
+set(fh,'defaultAxesYColor',param.axesColor);
+set(fh,'defaultAxesZColor',param.axesColor);
+
+set(fh,'defaultAxesFontSize',param.fontsize);
+set(fh,'defaultTextFontSize',param.fontsize);
+set(fh,'defaultAxesLineWidth', param.lineWidth);
+set(fh,'defaultLineLineWidth', param.lineWidth);
+set(fh,'DefaultLineMarkerSize', param.markerSize);
 
 set(fh,'DefaultAxesXGrid','off');
 set(fh,'DefaultAxesYGrid','off');
 set(fh,'DefaultAxesBox','off');
-% set(fh,'PaperPositionMode','auto');
+set(fh,'PaperPositionMode','auto');
 
 end
--- utility/plotTools/subplotInMM.m
+++ utility/plotTools/subplotInMM.m
@@ -1,7 +1,11 @@
-function h=subplotInMM(Xpos,Ypos,Width,Height,SetTop)
+function h=subplotInMM(Xpos,Ypos,Width,Height,SetTop,sumerimpose)
 
-if nargin<5
-    SetTop=false;
+if nargin<5 || isempty(SetTop)
+    SetTop=true;
+end
+
+if nargin<6 || isempty(sumerimpose)
+    sumerimpose=false;
 end
 
 unit=get(gcf,'paperunit');
@@ -19,12 +23,19 @@ scale=paperPos(3:4);
 % Ypos=paperPos(4)-paperPos(2)-Ypos;
 Ypos=paperPos(4)-Ypos;
 
-if SetTop;
-    ax=subplot('position',[Xpos/scale(1),Ypos/scale(2)-Height/scale(2),Width/scale(1),Height/scale(2)]);
+if ~sumerimpose
+    if SetTop
+        ax=subplot('position',[Xpos/scale(1),Ypos/scale(2)-Height/scale(2),Width/scale(1),Height/scale(2)]);
+    else
+        ax=subplot('position',[Xpos/scale(1),Ypos/scale(2),Width/scale(1),Height/scale(2)]);
+    end
 else
-    ax=subplot('position',[Xpos/scale(1),Ypos/scale(2),Width/scale(1),Height/scale(2)]);
+    if SetTop
+        ax=axes('position',[Xpos/scale(1),Ypos/scale(2)-Height/scale(2),Width/scale(1),Height/scale(2)]);
+    else
+        ax=axes('position',[Xpos/scale(1),Ypos/scale(2),Width/scale(1),Height/scale(2)]);
+    end    
 end
-
 if nargout>0
     h=ax;
 end
